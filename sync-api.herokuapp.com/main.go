489bb268428dc1662dd4c2275d079e018a3d010c
killvung <killvung@gmail.com>
2021-04-14 16:26:31 -0500

    Initial commit

---
+++ sync-api.herokuapp.com/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be initialized")
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello world",
+		})
+	})
+	r.Run(":" + port)
+}
---
+++ sync-api.herokuapp.com/server/util.go
@@ -0,0 +1,5 @@
+package server
+
+func GetHelloWorld() string {
+	return "Hello world"
+}
